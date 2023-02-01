385b161cc102578817e1d94002ef41c5d281ece3
Martin Karugaba <martinkarugaba21@gmail.com>
2023-02-01 13:07:51 +0300

    complete task 1

---
+++ string.test.js
@@ -0,0 +1,15 @@
+const stringLength = require('./string');
+
+test('returns the length of a string', () => {
+  expect(stringLength('bob')).toBe(3);
+});
+
+test('check if input string is at least 1 character long', () => {
+  expect(() => stringLength('')).toThrow('The input string must be atleast 1 character long');
+});
+
+test('check if input string is longer than 10 characters', () => {
+  expect(() => stringLength('Hello there here')).toThrow(
+    'The input string must not be longer than 10 characters'
+  );
+});
